42d63f84ac7067979e6906ee2b6e549733396ec1
Marco Tulio <marcotulio_iub@hotmail.com>
2023-08-18 14:45:56 -0300

    Ajustes na aplicação

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,4 +1,14 @@
+import { IsNotEmpty, Length } from 'class-validator';
+
 export class CreateTaskDto {
+  @IsNotEmpty({
+    message: 'The title should not be empty',
+  })
+  @Length(5, 100)
   title: string;
+
+  @IsNotEmpty({
+    message: 'The description should not be empty',
+  })
   description: string;
 }
