75dfadb0cc42e7f24d5bc847205930fe2bf44de7
Marco Tulio <marcotulio_iub@hotmail.com>
2023-08-18 13:11:35 -0300

    CRUD created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
+import { PrismaService } from './database/prisma.service';
 
 @Module({
   imports: [TasksModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [PrismaService],
 })
 export class AppModule {}
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,26 +1,54 @@
 import { Injectable } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
+import { PrismaService } from 'src/database/prisma.service';
 
 @Injectable()
 export class TasksService {
-  create(createTaskDto: CreateTaskDto) {
-    return 'This action adds a new task';
+  constructor(private prisma: PrismaService) {}
+
+  async create({ title, description }: CreateTaskDto) {
+    const task = await this.prisma.tasks.create({
+      data: {
+        title,
+        description,
+      },
+    });
+    return task;
   }
 
-  findAll() {
-    return `This action returns all tasks`;
+  async findAll() {
+    const tasks = await this.prisma.tasks.findMany();
+
+    return tasks;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} task`;
+  async findOne(id: string) {
+    return this.prisma.tasks.findUnique({
+      where: {
+        id: id,
+      },
+    });
   }
 
-  update(id: number, updateTaskDto: UpdateTaskDto) {
-    return `This action updates a #${id} task`;
+  async update(id: string, { title, description }: UpdateTaskDto) {
+    const task = await this.prisma.tasks.update({
+      where: {
+        id: id,
+      },
+      data: {
+        title,
+        description,
+      },
+    });
+    return task;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} task`;
+  async remove(id: string) {
+    await this.prisma.tasks.delete({
+      where: {
+        id: id,
+      },
+    });
   }
 }
