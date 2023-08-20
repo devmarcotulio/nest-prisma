67b1146b73ac45a527ac0b682b8e060152eaa52a
Marco Tulio <marcotulio_iub@hotmail.com>
2023-08-20 00:58:03 -0300

    Ajustes na aplicação

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,4 +1,14 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
 export class CreateUserDto {
+  @IsNotEmpty({
+    message: 'The email should not be empty',
+  })
+  @IsEmail()
   email: string;
+
+  @IsNotEmpty({
+    message: 'The password should not be empty',
+  })
   password: string;
 }
