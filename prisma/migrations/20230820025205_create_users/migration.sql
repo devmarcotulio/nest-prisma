e5458c607fbf41639c6f6b8581f659a6c0d49aa7
Marco Tulio <marcotulio_iub@hotmail.com>
2023-08-19 23:52:21 -0300

    Model users

---
+++ prisma/migrations/20230820025205_create_users/migration.sql
@@ -0,0 +1,19 @@
+-- AlterTable
+ALTER TABLE "tasks" ADD COLUMN     "usersId" TEXT;
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- AddForeignKey
+ALTER TABLE "tasks" ADD CONSTRAINT "tasks_usersId_fkey" FOREIGN KEY ("usersId") REFERENCES "users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
